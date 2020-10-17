75b813ac5720b018cb5a912b4a6b70b24cef6da1
marcusteh1238 <marcusteh1238@gmail.com>
2020-10-18 02:04:02 +0800

    Add logger

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const { Client, Collection } = require("discord.js");
 
+const logger = require("./src/helpers/logger");
+
 const client = new Client({ disableMentions: "everyone" });
 client.login(process.env.DISCORD_TOKEN);
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    logger.info(`Logged in as ${client.user.tag}!`);
 });
